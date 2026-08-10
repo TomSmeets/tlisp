4ccc52a9fc4a49beda71cbc47fedb8505360e5dc
Tom Smeets <tom@tsmeets.nl>
2026-08-10 22:29:45 +0200

    work on eval

--- src/main.c
+++ src/main.c
@@ -45,13 +45,26 @@ static void *str_dup(size_t len, char *s) {
 typedef struct Expr Expr;
 
 struct Expr {
-    char *label;
-    Expr *car;
-    Expr *cdr;
+    enum {
+        Expr_None,
+        Expr_Label,
+        Expr_Num,
+        Expr_Cons,
+    } type;
+
+    union {
+        char *label;
+        long num;
+        struct {
+            Expr *car;
+            Expr *cdr;
+        };
+    };
 };
 
 static Expr *expr_cons(Expr *car, Expr *cdr) {
     Expr *e = mem_struct(Expr);
+    e->type = Expr_Cons;
     e->car = car;
     e->cdr = cdr;
     return e;
@@ -59,7 +72,15 @@ static Expr *expr_cons(Expr *car, Expr *cdr) {
 
 static Expr *expr_label(char *label) {
     Expr *e = mem_struct(Expr);
+    e->type = Expr_Label;
     e->label = label;
+    return e;
+}
+
+static Expr *expr_num(long num) {
+    Expr *e = mem_struct(Expr);
+    e->type = Expr_Num;
+    e->num = num;
     return e;
 }
 
@@ -86,6 +107,32 @@ static void parse_whitespace(Parse *p) {
     }
 }
 
+static long parse_long(Parse *p) {
+    bool negative = false;
+    long num = 0;
+
+    if(parse_peek(p) == '-') {
+        negative = true;
+        parse_next(p);
+    } else if(parse_peek(p) == '+') {
+        parse_next(p);
+    }
+
+    for(;;) {
+        char c = parse_peek(p);
+        if(c == 0) break;
+        if (!(c >= '0' && c <= '9')) break;
+
+        num *= 10;
+        num += c - '0';
+        parse_next(p);
+    }
+
+    if(negative) num = -num;
+    return num;
+}
+
+
 static Expr *parse_list(Parse *p);
 static Expr *parse_value(Parse *p);
 
@@ -99,7 +146,13 @@ static Expr *parse_value(Parse *p) {
         return parse_list(p);
     }
 
-    if (c == '\0') return 0;
+    if (c == '\0') {
+        return 0;
+    }
+
+    if ((c >= '0' && c <= '9') || ((c == '-' || c == '+') && (p->cursor[1] >= '0' && p->cursor[1] <= '9'))) {
+        return expr_num(parse_long(p));
+    }
 
     char *expr_start = p->cursor;
     for (;;) {
@@ -112,8 +165,7 @@ static Expr *parse_value(Parse *p) {
     }
     char *expr_end = p->cursor;
     char *label = str_dup(expr_end - expr_start, expr_start);
-    Expr *e = expr_label(label);
-    return e;
+    return expr_label(label);
 }
 
 // Returns a list
@@ -133,7 +185,6 @@ static Expr *parse_list(Parse *p) {
     if (c == '\0') {
         return 0;
     }
-
     Expr *car = parse_value(p);
     Expr *cdr = parse_list(p);
     Expr *ret = expr_cons(car, cdr);
@@ -149,9 +200,11 @@ static void pretty_list(Expr *e);
 static void pretty_value(Expr *e) {
     if (e == 0) {
         printf("()");
-    } else if (e->label) {
+    } else if (e->type == Expr_Label) {
         printf("%s", e->label);
-    } else {
+    } else if (e->type == Expr_Num) {
+        printf("%ld", e->num);
+    } else if (e->type == Expr_Cons) {
         printf("(");
         pretty_list(e);
         printf(")");
@@ -168,15 +221,50 @@ static void pretty_list(Expr *e) {
     }
 }
 
+static bool str_eq(char *a, char *b) {
+    for(;;) {
+        if(*a != *b) return false;
+        if (*a == 0) return true;
+        a++;
+        b++;
+    }
+}
+
+// ================
+// Eval
+// ================
+static Expr *eval_list(Expr *e) {
+    Expr *car = e->car;
+    char *label = e->car->label;
+
+    if(str_eq(label, "+")) {
+        long sum = 0;
+        Expr *it = e->cdr;
+        while(it) {
+            if(it->car->type != Expr_Num) {
+                printf("ERROR\n");
+                break;
+            }
+            sum += it->car->num;
+            it = it->cdr;
+        }
+        return expr_num(sum);
+    }
+
+    return e;
+}
+
 int main(void) {
-    const char code[] = "(a () b (c) (d e) ())";
+    const char code[] = "(+ 1 2 3 4  5)";
     printf("Code: %s\n", code);
 
     char *cursor = (char *)code;
     char *end = cursor + sizeof(code);
 
     Parse p = {.cursor = (char *)code};
-    Expr *e = parse_list(&p);
-    pretty_list(e);
+    Expr *e = parse_value(&p);
+    pretty_value(e);
+    printf("\n");
+    pretty_value(eval_list(e));
     printf("\n");
 }
