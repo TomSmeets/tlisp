dbb91906c3c4cc200fd9650c014568c4435fea86
Tom Smeets <tom@tsmeets.nl>
2026-09-24 20:31:35 +0200

    improve output

--- aoc/2025/01/main.lisp
+++ aoc/2025/01/main.lisp
@@ -1,11 +1,11 @@
 (def parsenum (s) (do
-    (let n 0)
-    (while (not (nil? s)) (do
-       (let x (car s))
-       (set n (add (mul n 10) (sub x '0')))
-       (set s (cdr s))
-    ))
-    n
+  (let n 0)
+  (while (not (nil? s)) (do
+    (let x (car s))
+    (set n (add (mul n 10) (sub x (car "0"))))
+    (set s (cdr s))
+  ))
+  n
 ))
 
 (def split (xs ys) (do
--- src/input.lisp
+++ src/input.lisp
@@ -2,13 +2,13 @@
   (print "parsenum" s)
   (let n 0)
   (while (not (nil? s)) (do
-    (print "s" s)
+    (print s n)
     (let x (car s))
     (set n (add (mul n 10) (sub x (car "0"))))
     (set s (cdr s))
-    (print "n" n)
   ))
+  (print s n)
   n
 ))
 
-(parsenum "12356")
+(parsenum "12356789123456789")
