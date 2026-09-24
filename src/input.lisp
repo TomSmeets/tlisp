cf674fec7277c879154021bfdcea5c67322ad42b
Tom Smeets <tom@tsmeets.nl>
2026-09-24 20:29:44 +0200

    fix parsenum

--- src/input.lisp
+++ src/input.lisp
@@ -1,12 +1,14 @@
 (def parsenum (s) (do
-    (print "parsenum")
-    (let n 0)
-    ; (while (not (nil? s)) (do
-    ;    (let x (car s))
-    ;    (set n (add (mul n 10) (sub x '0')))
-    ;    (set s (cdr s))
-    ; ))
-    n
+  (print "parsenum" s)
+  (let n 0)
+  (while (not (nil? s)) (do
+    (print "s" s)
+    (let x (car s))
+    (set n (add (mul n 10) (sub x (car "0"))))
+    (set s (cdr s))
+    (print "n" n)
+  ))
+  n
 ))
 
-(parsenum "123")
+(parsenum "12356")
